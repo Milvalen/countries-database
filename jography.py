e30f313194dba2700f7871761cca933b6c204634
Milvalen <artemyerkov@gmail.com>
2023-02-19 09:56:09 +0300

    added message if country not in base

--- jography.py
+++ jography.py
@@ -5,61 +5,40 @@ import requests
 def exchange():
     exc = data['rates']['RUB'] / data['rates'][l[3]]
     global round_exc
-    round_exc = round(exc, 2)
+    round_exc = round(exc, 2) # функция, считающая курс валюты
 def population():
     for line in pop:
         p = line.split(",")
         if l[4] == p[2]:
-            print(f"Население в стране {country}: {p[3]} человек") # функция, выдающая население страны
+            print(f"Население в стране {country}: {p[3]} человек\n")
 
 
-try:
-    pd.read_html(requests.get('https://www.worldometers.info/world-population/population-by-country/').content)[-1].to_csv('pop.csv') 
-    # таблица с населением
+pd.read_html(requests.get('https://www.worldometers.info/world-population/population-by-country/').content)[-1].to_csv('pop.csv') 
 
 
-    url = 'https://api.exchangerate.host/latest'
-    response = requests.get(url)
-    data = response.json() # API с кодами валют и их курсом
+url = 'https://api.exchangerate.host/latest' # API с кодами валют и их курсом
+response = requests.get(url)
+data = response.json() 
 
 
-    while True:
-        country = input("Введите страну: ")
+while True:
+    country = input("Введите страну: ")
+    
+    
+    # цикл с файлами
+    with open("countries.txt", "r", "utf_8_sig") as country_file, open("pop.csv", "r", "utf_8_sig") as pop: 
+
         
-        
-        with open("africa.txt", "r", "utf_8_sig") as africa, open("america.txt", "r", "utf_8_sig") as america, \
-            open("australia.txt", "r", "utf_8_sig") as australia, open("eurasia.txt", "r", "utf_8_sig") as eurasia, \
-            open("pop.csv", "r", "utf_8_sig") as pop: # цикл с файлами
-
-            
-            for line in africa:
-                if country.title() in line:
-                    l = line.split(", ")
-                    exchange()
-                    if country.title() == l[0]:
-                        print(f"Страна {country} находится в Африке. Столица: {l[1]}. Валюта: {l[2]}. Курс валюты к рублю: {round_exc}")
-                        population()
-            for line in america:
-                if country.title() in line:
-                    l = line.split(", ")
-                    exchange()
-                    if country.title() == l[0]:
-                        print(f"Страна {country} находится в Америке. Столица: {l[1]}. Валюта: {l[2]}. Курс валюты к рублю: {round_exc}.")
-                        population()
-            for line in australia:
-                if country.title() in line:
-                    l = line.split(", ")
-                    exchange()
-                    if country.title() == l[0]:
-                        print(f"Страна {country} находится в Австралии. Столица: {l[1]}. Валюта: {l[2]}. Курс валюты к рублю: {round_exc}")
-                        population()
-            for line in eurasia:
-                if country.title() in line:
-                    l = line.split(", ")
-                    exchange()
-                    if country.title() == l[0]:
-                        print(f"Страна {country} находится в Евразии. Столица: {l[1]}. Валюта: {l[2]}. Курс валюты к рублю: {round_exc}")
-                        population() # основной цикл   
-except: 
-    print("Нет подключения к интернету")
-    input()
+        # основной цикл  
+        for line in country_file:
+            if country.title() in line:
+                l = line.split(", ")
+                exchange()
+                if country.title() == l[0]:
+                    print(f"\nСтрана {country} находится в {l[5]}. Столица: {l[1]}. Валюта: {l[2]}. Курс валюты к рублю: {round_exc}")
+                    population()
+    
+    
+    # если страна не найдена
+    if country.title() != l[0]:
+        print(f"\nСтраны {country} нет в базе\n")
